262cef8803d6a23dde39f03fa9a1bcd123d66cb9
Simon Kennedy <sffjunkie+code@gmail.com>
2025-06-30 09:48:56 +0100

    Rename unittest shell function

--- devshell/nix/scripts/unittest.nix
+++ devshell/nix/scripts/unittest.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  script = pkgs.writeScriptBin "nix-unittest" ''
+  script = pkgs.writeScriptBin "nut" ''
     #!${lib.getExe pkgs.bash}
     $(nix flake show --json 2>/dev/null | jq -e .unitTests >/dev/null)
     if [ $? -eq 0 ]; then
