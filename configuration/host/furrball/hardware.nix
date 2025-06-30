5612eaf4cadb6305fa02cf50e0801f703915f852
Simon Kennedy <sffjunkie+code@gmail.com>
2025-06-30 09:49:15 +0100

    Nest under config attr

--- configuration/host/furrball/hardware.nix
+++ configuration/host/furrball/hardware.nix
@@ -5,7 +5,9 @@
   ...
 }:
 {
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.enableRedistributableFirmware = true;
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  config = {
+    powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+    hardware.enableRedistributableFirmware = true;
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
