a70233254bef740f0f17afa185c819be34748482
Simon Kennedy <sffjunkie+code@gmail.com>
2025-07-01 20:37:59 +0100

    Set host platform

--- installer/looniversity-minimal.nix
+++ installer/looniversity-minimal.nix
@@ -9,6 +9,8 @@
 with lib;
 {
   config = {
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
     sops = {
       defaultSopsFile = ../configuration/secret/secrets.yaml;
       defaultSopsFormat = "yaml";
