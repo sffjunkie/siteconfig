7026390fdaf7df3739c538b6b33a23063d11b145
Simon Kennedy <sffjunkie+code@gmail.com>
2025-08-03 19:21:01 +0100

    Update module

--- configuration/module/host/www/netbox/default.nix
+++ configuration/module/host/www/netbox/default.nix
@@ -17,16 +17,28 @@ in
   config = mkIf cfg.enable {
     sops.secrets."netbox/secret_key" = {
       sopsFile = config.sopsFiles.service;
+      owner = config.users.users.netbox.name;
+      group = config.users.users.netbox.group;
     };
 
     services.netbox = {
       enable = true;
+      port = 9001;
+      listenAddress = "0.0.0.0";
       package = pkgs.netbox_4_3;
       secretKeyFile = config.sops.secrets."netbox/secret_key".path;
+      settings = {
+        CSRF_TRUSTED_ORIGINS = [
+          "http://furrball.looniversity.net"
+          "http://localhost"
+        ];
+        AUTH_PASSWORD_VALIDATORS = [ ];
+      };
     };
 
     services.nginx = {
       enable = true;
+
       user = "netbox";
       recommendedTlsSettings = true;
       clientMaxBodySize = "25m";
@@ -34,11 +46,11 @@ in
       virtualHosts."${config.networking.fqdn}" = {
         locations = {
           "/" = {
-            proxyPass = "http://[::1]:8001";
-            # proxyPass = "http://${config.services.netbox.listenAddress}:${config.services.netbox.port}";
+            # proxyPass = "http://0.0.0.0:8001";
+            proxyPass = "http://${config.services.netbox.listenAddress}:${toString config.services.netbox.port}";
           };
           "/static/" = {
-            alias = "${config.services.netbox.dataDir}/static/";
+            root = "${config.services.netbox.dataDir}";
           };
         };
         # forceSSL = true;
