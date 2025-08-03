d304a314609503a67e5038bfd2d4ffbed73dea65
Simon Kennedy <sffjunkie+code@gmail.com>
2025-08-03 08:42:52 +0100

    Remove password configuration

--- configuration/user/nixos/host/default.nix
+++ configuration/user/nixos/host/default.nix
@@ -9,12 +9,6 @@ let
 in
 {
   config = {
-    # https://github.com/Mic92/sops-nix#setting-a-users-password
-    sops.secrets."${username}/password_hash" = {
-      neededForUsers = true;
-      sopsFile = config.sopsFiles.user;
-    };
-
     users.users.${username} = {
       isNormalUser = true;
       uid = 1100;
@@ -27,7 +21,6 @@ in
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8Ayl5f2l+BFrUTzbyzmiLbhmYzTaLptCwe80Vk84NJ ${username}"
         ];
       };
-      hashedPasswordFile = config.sops.secrets."${username}/password_hash".path;
     };
 
     services.openssh.settings.AllowUsers = [ username ];
