eb47e41be81ba332341e5ecbb7e1e26c51c29b07
Simon Kennedy <sffjunkie+code@gmail.com>
2025-08-06 12:10:16 +0100

    Conditionally apply settings

--- configuration/module/user/editor/vscode/ai/default.nix
+++ configuration/module/user/editor/vscode/ai/default.nix
@@ -1,17 +1,35 @@
 {
-  programs.vscode = {
-    profiles = {
-      default = {
-        userSettings = {
-          "dataWrangler.experiments.copilot.enabled" = false;
-          "chat.agent.enabled" = false;
-          "chat.extensionTools.enabled" = false;
-          "chat.focusWindowOnConfirmation" = false;
-          "chat.setupFromDialog" = false;
-          "remote.SSH.experimental.chat" = false;
-          "chat.commandCenter.enabled" = false;
-          "gitlab.duoChat.enabled" = false;
-          "workbench.editor.empty.hint" = "hidden";
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.looniversity.editor.vscode.ai;
+  inherit (lib) mkDefault mkEnableOption mkIf;
+in
+{
+  options.looniversity.editor.vscode.ai = {
+    enable = mkEnableOption "vscode ai configuration";
+    default = false;
+  };
+
+  config = mkIf (!cfg.enable) {
+    programs.vscode = {
+      profiles = {
+        default = {
+          userSettings = {
+            "dataWrangler.experiments.copilot.enabled" = false;
+            "chat.agent.enabled" = false;
+            "chat.extensionTools.enabled" = false;
+            "chat.focusWindowOnConfirmation" = false;
+            "chat.setupFromDialog" = false;
+            "remote.SSH.experimental.chat" = false;
+            "chat.commandCenter.enabled" = false;
+            "gitlab.duoChat.enabled" = false;
+            "workbench.editor.empty.hint" = "hidden";
+          };
         };
       };
     };
