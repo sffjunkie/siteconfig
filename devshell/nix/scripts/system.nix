7d03d7b31179896da0c54b7be02182c8b04e96eb
Simon Kennedy <sffjunkie+code@gmail.com>
2025-09-07 11:04:07 +0100

    Add 'sudo'

--- devshell/nix/scripts/system.nix
+++ devshell/nix/scripts/system.nix
@@ -68,14 +68,15 @@ let
         ;;
 
       boot)
+        sudo
         header "$1"
-
         sudo nixos-rebuild boot --flake ".#''${target}" \
           --impure --log-format internal-json "''${extra_args[@]}" |& \
           ${pkgs.nix-output-monitor}/bin/nom --json
         ;;
 
       switch)
+        sudo
         header "$1"
         sudo nixos-rebuild switch --flake ".#''${target}" \
           --impure --log-format internal-json "''${extra_args[@]}" |& \
